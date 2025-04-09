f7ec2aeaf3f4155cee19cdee50543cb100294c0e
David Byron <davbyron@icloud.com>
2025-04-08 21:42:09 -0400

    remove board square change_type function

--- board_square.gd
+++ board_square.gd
@@ -1,22 +1,6 @@
 extends Node2D
 class_name BoardSquare
 
-enum SquareType { DIRT, WATER, GRASS, LAVA }
-
-@export var type: SquareType = SquareType.DIRT
-@export var occupied_by: Node = null  # Can store a card or unit
-
-func change_type(new_type: SquareType):
-	type = new_type
-	update_visual()
-
 func update_visual():
 	# Change the sprite, color, or other visuals based on type
 	$Sprite2D.texture = preload("res://assets/sludge_pool.png")
-	#match type:
-		#SquareType.DIRT:
-			#$Sprite2D.texture = preload("res://textures/dirt.png")
-		#SquareType.WATER:
-			#$Sprite2D.texture = preload("res://textures/water.png")
-		#_:
-			#$Sprite2D.texture = preload("res://textures/default.png")
