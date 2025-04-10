ec8f2d7bcdb6b21f5c2170f9dafdba85858ae889
David Byron <davbyron@icloud.com>
2025-04-09 20:27:55 -0400

    update CardData and add pawnA and pawnB

--- CardData.gd
+++ CardData.gd
@@ -3,5 +3,9 @@ class_name CardData
 
 @export var name: String
 @export var image: Texture2D
-@export var damage: int
-@export var description: String
+@export var level: int
+@export var attack: int
+@export var health: int
+@export var attackPattern: String
+@export var movementPattern: String
+@export var description: String = ""
